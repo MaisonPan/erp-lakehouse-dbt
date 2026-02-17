e369934b4a77ed416ed9712b2d0eb9c9f09cc72d
Hongwei Pan <chinaswiss@gmail.com>
2026-02-17 13:38:08 +0000

    1

--- models/bronze/northwind/bronze_orders.sql
+++ models/bronze/northwind/bronze_orders.sql
@@ -1,6 +1,15 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='incremental'
+) }}
 
 SELECT
     *,
-    regexp_extract(input_file_name(), '/Orders/([0-9]{4}-[0-9]{2}-[0-9]{2})/', 1) AS load_date
+    regexp_extract(input_file_name(),
+        '/Orders/([0-9]{4}-[0-9]{2}-[0-9]{2})/',1) AS load_date
 FROM parquet.`abfss://landing@panmaisonadls.dfs.core.windows.net/northwind/Orders/*/Orders`
+
+{% if is_incremental() %}
+WHERE regexp_extract(input_file_name(),
+        '/Orders/([0-9]{4}-[0-9]{2}-[0-9]{2})/',1)
+      > (SELECT max(load_date) FROM {{ this }})
+{% endif %}
