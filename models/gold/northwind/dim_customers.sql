262ef95cf4e39b36097820cf90d9402b841d7c52
Hongwei Pan <chinaswiss@gmail.com>
2026-02-24 10:34:05 +0000

    6

---
+++ models/gold/northwind/dim_customers.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+select
+  customer_id,
+  company_name,
+  contact_name,
+  contact_title,
+  address,
+  city,
+  region,
+  postal_code,
+  country,
+  phone,
+  fax
+from {{ ref('stg_customers') }}
