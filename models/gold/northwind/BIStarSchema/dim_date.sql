dfe10fdbebd6b59eed7e8e7f2e0c0f644390d292
Hongwei Pan <chinaswiss@gmail.com>
2026-03-05 09:05:25 +0000

    2

--- models/gold/northwind/BIStarSchema/dim_date.sql
+++ models/gold/northwind/BIStarSchema/dim_date.sql
@@ -13,13 +13,14 @@ dates as (
 )
 
 select
-  date_day                                   as date_day,
-  year(date_day)                              as year,
-  quarter(date_day)                           as quarter,
-  month(date_day)                             as month,
-  date_format(date_day, 'yyyy-MM')            as year_month,
-  weekofyear(date_day)                        as week_of_year,
-  dayofmonth(date_day)                        as day_of_month,
-  date_format(date_day, 'EEEE')               as day_name,
+  date_day                           as date_day,
+  year(date_day)                     as year,
+  quarter(date_day)                  as quarter,
+  month(date_day)                    as month,
+  date_format(date_day,'MMM')        as month_short,
+  date_format(date_day, 'yyyy-MM')   as year_month,
+  weekofyear(date_day)               as week_of_year,
+  dayofmonth(date_day)               as day_of_month,
+  date_format(date_day, 'EEEE')      as day_name,
   case when dayofweek(date_day) in (1,7) then true else false end as is_weekend
 from dates
